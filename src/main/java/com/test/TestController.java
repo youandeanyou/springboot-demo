4e2ef0559188a5debdc591ef352823511c37d930
test <test@test.com>
2022-05-30 10:38:22 +0800

    test

--- src/main/java/com/test/TestController.java
+++ src/main/java/com/test/TestController.java
@@ -1,11 +1,15 @@
 package com.test;
 
+import com.alibaba.fastjson2.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
+@Slf4j
 @RestController
 public class TestController {
 
@@ -14,7 +18,12 @@ public class TestController {
 
         TestOutput output = new TestOutput();
         output.setCurrent(LocalDateTime.now());
-        output.setHistory(LocalDate.now());
+        output.setHistory(new Date());
+        output.setYesterday(LocalDate.now());
+        output.setTomorrow(new java.sql.Date(System.currentTimeMillis()));
+        // 这里的打印，JSONField是生效的
+        log.info("结果 - [output - {}]", JSON.toJSONString(output));
+        // 这里直接返回，得到的结果是以FastJsonConfig.setDateFormat为准的
         return output;
 
     }
--- src/main/java/com/test/TestOutput.java
+++ src/main/java/com/test/TestOutput.java
@@ -1,14 +1,21 @@
 package com.test;
 
+import com.alibaba.fastjson2.annotation.JSONField;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 public class TestOutput {
 
     private LocalDateTime current;
-    private LocalDate history;
+    @JSONField(format = "yyyy-MM-dd HH")
+    private Date history;
+    @JSONField(format = "yyyy-MM-dd")
+    private LocalDate yesterday;
+    @JSONField(format = "yyyy-MM-dd HH:mm")
+    private java.sql.Date tomorrow;
 
 }
